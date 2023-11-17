a1e3cbafae56b74f79ca5553fbe1931c1769991a
Ismael Avila <ismaelg.avilag@gmail.com>
2023-11-17 12:15:33 -0600

    Se agregó la funcionalidad al botón para abrir el repositorio en GitHub

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
+
 from main_window import *
 
 class Ui_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        self.pushButtonGitHub.clicked.connect(self.open_github_repo)
+
+    def open_github_repo(self):
+        repo_url = "https://github.com/ismaelg-avilag/spam-classifier"
+        QDesktopServices.openUrl(QUrl(repo_url))
 
 
 if __name__ == "__main__":

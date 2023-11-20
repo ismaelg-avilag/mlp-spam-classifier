72de269614c3feed2494c531586eb195036cbdf5
Ismael Avila <ismaelg.avilag@gmail.com>
2023-11-20 14:15:45 -0600

    Se muestra la cantidad de ham y spam en los archivos.
    - Se dió funcionalidad al botón de selección de archivos en la ventana principal
    - Se abre una ventana de dialogo para seleccionar una carpeta
    - Se procesan todos los archivos de la carpeta
    - Cuando se finaliza el procesamiento, se muestra la cantidad de ham y spam

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QDesktopServices
 
 from main_window import *
 from about_window import *
+from evaluateEmails import *
 
 class Ui_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
@@ -11,6 +12,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.pushButtonGitHub.clicked.connect(self.open_github_repo)
         self.pushButtonAboutProject.clicked.connect(self.open_about_project_window)
+        self.pushButtonSelectFiles.clicked.connect(self.select_directory)
 
     def open_github_repo(self):
         repo_url = "https://github.com/ismaelg-avilag/spam-classifier"
@@ -21,6 +23,14 @@ class Ui_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui = Ui_AboutWindow()
         self.ui.setupUi(self.about_window)
         self.about_window.show()
+
+    def select_directory(self):
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Selecciona una carpeta")
+        
+        if directory:
+            amountOfSpam, amountOfHam = evaluate(directory)
+            self.labelAmountOfHam.setText("Los archivos procesados tienen un " + str(amountOfHam) + "% Ham")
+            self.labelAmountOfSpam.setText("Los archivos procesados tienen un  " +  str(amountOfSpam) + "% Spam")
 
 
 if __name__ == "__main__":

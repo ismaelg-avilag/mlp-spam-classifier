0cb887d15a773bcd4cf0facc394a76cf69a21d05
Ismael Avila <ismaelg.avilag@gmail.com>
2023-11-20 15:08:19 -0600

    Se agregó progress bar
    - Se agregó un progress a la ventana principal
    - El progress bar muestra el progreso del procesamiento de los archivos
    - Una vez que termina de cargar, se muestra la cantidad de ham y spam

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QDesktopServices
 
 from main_window import *
 from about_window import *
+from progress_bar import *
 from evaluateEmails import *
 
 class Ui_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -28,9 +29,22 @@ class Ui_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Selecciona una carpeta")
         
         if directory:
-            amountOfSpam, amountOfHam = evaluate(directory)
-            self.labelAmountOfHam.setText("Los archivos procesados tienen un " + str(amountOfHam) + "% Ham")
-            self.labelAmountOfSpam.setText("Los archivos procesados tienen un  " +  str(amountOfSpam) + "% Spam")
+            self.labelSelectedDirectory.setText("Carpeta seleccionada: " + directory)
+            self.progressBar.setValue(0)
+            self.progressBar.setVisible(True)
+            self.worker_thread = WorkerThread(directory)
+            self.worker_thread.update_progress.connect(self.update_progress_bar)
+            self.worker_thread.finished_processing.connect(self.showResults)
+            self.worker_thread.start()
+
+            self.amountOfSpam, self.amountOfHam = evaluate(directory)
+
+    def update_progress_bar(self, progress):
+        self.progressBar.setValue(progress)
+    
+    def showResults(self):
+        self.labelAmountOfHam.setText("Los archivos procesados tienen un " + str(self.amountOfHam) + "% Ham")
+        self.labelAmountOfSpam.setText("Los archivos procesados tienen un  " +  str(self.amountOfSpam) + "% Spam")
 
 
 if __name__ == "__main__":
---
+++ progress_bar.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+import time
+import os
+
+class WorkerThread(QThread):
+    update_progress = pyqtSignal(int)
+    finished_processing = pyqtSignal()
+
+    def __init__(self, directory):
+        super().__init__()
+        self.directory = directory
+    
+    def run(self):
+        files = os.listdir(self.directory)
+        amountOfFiles = len(files)
+        
+        for i, file in enumerate(files):
+            time.sleep(0.1)
+            progress = int((i + 1) * 100 / amountOfFiles)
+            self.update_progress.emit(progress)
+        
+        self.finished_processing.emit()
